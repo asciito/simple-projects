d1b4f85d91604c056399582f93c19ac11444c383
Ayax Córdova <hcordova@blueoceansustainability.com>
2021-11-04 01:25:00 -0600

    Add new Component Project
    Basic structure for a Vue Component called Vue-CRUD

---
+++ vue/vue-crud/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>hello, world!</h1>
+
+	<Table
+		:columns="['name', 'last_name']"
+		:data="[
+			{ name: 'Ayax', last_name: 'Córdova' },
+			{ name: 'Eunice', last_name: 'Ramos' },
+		]"
+	/>
+</template>
+
+<script>
+import Table from './components/Table.vue';
+export default {
+    name: "App",
+    components: { Table },
+};
+</script>
+
+<style></style>
---
+++ vue/vue-crud/src/components/Table.vue
@@ -0,0 +1,51 @@
+<template>
+    <table>
+        <thead>
+            <tr v-if="columns.length">
+                <th>Actions</th>
+                <th v-for="col of columns" :key="col">{{ col }}</th>
+            </tr>
+        </thead>
+
+        <!-- Data v-for -->
+        <tbody>
+            <tr v-for="row of data" :key="row.id">
+                <td>
+                    Actions
+                    <button>lorem 1</button>
+                    <button>lorem 2</button>
+                </td>
+
+                <td v-for="(value, key) of row" :key="key">{{ value }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    props: {
+        columns: {
+            type: Array,
+            required: true,
+        },
+        data: {
+            type: Object,
+            require: true,
+            default: () => {},
+        }
+    },
+
+    components: {},
+
+    setup () {
+
+        return {
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
